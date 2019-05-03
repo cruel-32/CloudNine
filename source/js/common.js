b6cf84d64e30c95e286901f22b4349b7c9eaca46
cruel-32 <cruel32@nate.com>
2019-05-03 16:10:52 +0900

    modify

--- source/js/common.js
+++ source/js/common.js
@@ -13,31 +13,27 @@ const getDevice = width=> Object.keys(responsive).find(size => width < responsiv
 //메인에 큰 이미지영역의 높이만큼 탭영역 높이도 맞춰주어야 합니다. (resize 시에도)
 //일단 jquery로 짜두긴 했는데 추후에 angular 코드로 바꿔주셔도 됩니다.
 
-const $mediaSection = $('.media-section');
-const $tabWrap = $('.tab-wrap');
-$tabWrap.height($mediaSection.height())
-
 $(window).resize((e)=>{
     const device = getDevice($(window).width());
 
-    $tabWrap.height($mediaSection.height())
     switch(device){
         case 'xl' :
-            $tabWrap.height($mediaSection.height())
         break;
         case 'lg' :
-            $tabWrap.height($mediaSection.height())
         break;
         case 'md' :
-            $tabWrap.height($mediaSection.height())
         break;
         case 'sm' :
-            $tabWrap.height('auto')
         break;
         case 'xs' :
-            $tabWrap.height('auto')
         break;
     }
 });
 
-AOS.init();
+$(function () {
+    AOS.init();
+    $('[data-toggle="tooltip"]').tooltip({
+        boundary: 'window'
+    })
+})
+
